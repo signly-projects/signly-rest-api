ad8025b1e72ed7f0e5f09251f2e022595cd8cb3e
Alex Malhao <alex@signly.co>
2019-05-16 23:38:45 +0100

    Database config file adjustments

--- util/database.js
+++ util/database.js
@@ -1,19 +1,20 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 
-
 const mongoConnect = callback => {
   MongoClient.connect(
     `mongodb://${process.env.COSMOSDB_ACCOUNT}:${process.env.COSMOSDB_KEY}@${process.env.COSMOSDB_ACCOUNT}.documents.azure.com:${process.env.COSMOSDB_PORT}/?ssl=true`,
     { useNewUrlParser: true }
   )
-  .then(client => {
-    console.log('Connected.')
-    callback(client)
-  })
-  .catch(err => {
-    console.log(err)
-  })
+    .then(client => {
+      // eslint-disable-next-line no-console
+      console.log('Connected.')
+      callback(client)
+    })
+    .catch(err => {
+      // eslint-disable-next-line no-console
+      console.log(err)
+    })
 }
 
 module.exports = mongoConnect
