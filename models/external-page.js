fcb267c407b8566126ef8419a86ceb21cac0139b
Alex Malhao <alex@signly.co>
2019-07-10 11:25:42 +0100

    External page object updated to include mediaBlocks

--- models/external-page.js
+++ models/external-page.js
@@ -1,22 +1,28 @@
+class MediaBlock {
+  constructor (textSegment) {
+    this.rawText = textSegment
+  }
+}
+
 class ExternalPage {
   constructor (url, title) {
     this.url = url
     this.title = title
-    this.textSegments = []
+    this.mediaBlocks = []
     this.textSegmentCounter = 0
     this.wordCounter = 0
   }
 
   addTextSegment (textSegment) {
-    if (!this.textSegments.includes(textSegment)) {
-      this.textSegments.push(textSegment)
+    if (!this.mediaBlocks.some(mb => mb.textSegment === textSegment )) {
+      this.mediaBlocks.push(new MediaBlock(textSegment))
       this.textSegmentCounter += 1
       this.wordCounter += textSegment.split(' ').length
     }
   }
 
-  textSegments () {
-    return this.textSegments
+  mediaBlocks () {
+    return this.mediaBlocks
   }
 }
 
