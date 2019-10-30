8e8c7bb42a9a23934511622b93cf00b2ab62d5f5
Alex Malhao <alex@signly.co>
2019-10-30 14:13:10 +0000

    Redis config added.

--- startup/prod.js
+++ startup/prod.js
@@ -1,7 +1,7 @@
 const helmet = require('helmet')
 const compression = require('compression')
 
-exports = function (app) {
+module.exports = function (app) {
   app.use(helmet())
   app.use(compression())
 }
